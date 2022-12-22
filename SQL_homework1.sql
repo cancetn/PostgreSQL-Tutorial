285de436116eddfc483aede23cf5412213891ac8
Can Çetin <108129872+cancetn@users.noreply.github.com>
2022-12-22 16:16:15 +0300

    Update SQL_homework1.sql

--- SQL_homework1.sql
+++ SQL_homework1.sql
@@ -1,4 +1,4 @@
-#Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+# Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
 
 1-film tablosunda bulunan title ve description sütunlarındaki verileri sıralayınız.
 2-film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length) 60 dan büyük VE 75 ten küçük olma koşullarıyla sıralayınız.
@@ -6,7 +6,7 @@
 4-customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
 5-film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
 
-#Cevaplar
+# Cevaplar
 
 1- SELECT title, description FROM film;
 
